b55414c01f044215fbb0d64aba4bf92abdec1f5b
Moein Shafi <mosafer.moein@gmail.com>
2020-06-11 01:18:15 +0430

    Fixing zombie problem.

--- shm_test.c
+++ shm_test.c
@@ -22,12 +22,7 @@ int main(int argc, char const *argv[])
 	printf(1, "pid: %d,        page_id: 8,  result: %x\n\n", pid, (void*)shmget(8));
 	printf(1, "pid: %d,        page_id: 1,  result: %x\n\n", pid, (void*)shmget(1));
 
-    int j = 0;
-    for (int i = 0; i < 999999999; i++)
-    {
-        j++;
-    }
-
+    wait();
 	printf(1, ">>>> dead father.\n");
 	exit();
 }
